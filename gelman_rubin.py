e649b27ad1d3df0d5b1d73614de9d324af2e4aae
ArneTillmann <arne.tillmann.vellmar@gmail.com>
2020-12-16 12:54:52 +0100

    Revised with use off pep8. May still not be fully consistend though

--- gelman_rubin.py
+++ gelman_rubin.py
@@ -1,38 +1,73 @@
 import numpy as np
+"""This is the implementation of the revised Gelman-Rubin Diagnostic
+with use of the presented calculations given in the paper
+https://arxiv.org/pdf/1812.09384.pdf
+written by Dootika Vats and Christina Knudson
+"""
 
 def k_th_batch_mean(k, b, chains):
+    """calculation of a special mean for each chain respectively"""
+
     return (1.0/b) * chains[int((k-1)*b+1):int(k*b),:].sum(axis=0)
 
+
 def combined_estimator_of_mu(m, chains):
+    """The mean of means for different chains"""
+
     return 1.0/m * (chains.mean(axis=1)).sum(axis=0)
 
-def replicated_batch_means_estimator(b, a, m, chains):
+
+def replicated_batch_means_estimator(b, a, m, p, chains):
+    """intermediate step for the following calculation"""
+
     mu = combined_estimator_of_mu(m, chains)
-    s = len(chains[0,0])
-    x = np.zeros((s,s))
+    x = np.zeros((p,p))
+
     for i in range(m):
         for k in range(a):
             k_th_batch_m = k_th_batch_mean(k, b, chains[i])
-            #print(k_th_batch_m)
             x += np.outer((k_th_batch_m - mu),(k_th_batch_m - mu))
-    return (b/(a*m -1))*x
 
-def replicated_lugsail_batch_mean_sestimator(b, a, m, chains):
-    return 2*replicated_batch_means_estimator(float(b), a, m, chains) - replicated_batch_means_estimator(np.floor(float(b/3)), a, m, chains)
+    return (b/(a*m-1)) * x
+
+
+def replicated_lugsail_batch_mean_sestimator(b, a, m, p, chains):
+    """see equation (9) in the paper named at the beginning"""
+
+    return (2 * replicated_batch_means_estimator(float(b), a, m, p, chains)
+           - replicated_batch_means_estimator(np.floor(b/3), a, m, p, chains))
+
 
 #def biased_from_above_estimator(b, a, m, n, chains):
-#    return (float(n-1.0))/n * average_of_the_m_sample_variances(m, n, chains) + (replicated_lugsail_batch_mean_sestimator(b, a, m, chains))/float(n)
 
-def improved_estimator_PSRF(b, a, m, n, chains):
-    s = len(chains[0,0])
-    #print(replicated_batch_means_estimator(b,a,m,chains))
-    #print(np.linalg.det(replicated_lugsail_batch_mean_sestimator(b, a, m, chains)))
-    #print(np.linalg.det(average_of_the_m_sample_variances(m, n, chains)))
-    return ((n-1.)/n+(np.linalg.det(replicated_lugsail_batch_mean_sestimator(b, a, m, chains)/np.linalg.det(average_of_the_m_sample_variances(m, n, chains))))**(1./s)/n)**(1.0/2)
+#    return (float(n-1.0))/n * average_of_the_m_sample_covariances(m, n, chains)
+#           + (replicated_lugsail_batch_mean_sestimator(b, a, m, chains))/float(n)
 
-def average_of_the_m_sample_variances(m, n, chains):
-    s= len(chains[0,0])
-    x = np.zeros((s,s))
+
+def improved_estimator_PSRF(b, a, m, n, p, chains):
+    """The final revised gelman-rubin statistic receiving b,a as additional
+    parameters for the calculation of the k_th_batch_mean.
+    They are chosen in the way, that n=a*b, whereas n is the chain length
+    (number of samples in one chain). Usual choices for b include round off
+    n^(1/2) or round off n^(1/3) according to the authers.
+    m is the number of chains, n their length, p the dimension of the target
+    distribution and chains the np.ndarray containing the chains in order [m,n,p]
+    """
+
+    t_hat = replicated_lugsail_batch_mean_sestimator(b, a, m, p, chains)
+    sigma_hat = average_of_the_m_sample_covariances(m, n, p, chains)
+
+    return (((n-1.)/n
+           + (np.linalg.det(t_hat)
+           / np.linalg.det(sigma_hat))**(1./p)/n)**(1.0/2))
+
+
+def average_of_the_m_sample_covariances(m, n, p, chains):
+    """Auxiliary function"""
+
+    x = np.zeros((p,p))
+
     for i in range(m):
         x += np.cov(chains[i].T, ddof=1)
-    return 1./m*x
+
+    return 1./m * x
--- hmc_sampler.py
+++ hmc_sampler.py
@@ -1,47 +1,66 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-np.random.seed(42)
 import copy
+
+np.random.seed(42)
 
 
 class HMC:
-    def __init__(self, x_0, chain_length, log_prob, gradient_log_prob, stepsize, trajectory_length):
-        self.x_0 = x_0
-        self.chain_length = chain_length
+
+    def __init__(self, log_prob, gradient_log_prob,
+                 stepsize, trajectory_length):
+        """Class to create a HMC object, from which we can build chains.
+        Therefore we need a targeted distribution given as the log probabillity
+        (log density function), its log gradient and a stepsize and
+        trajectory length for the integrator simulating the Hamiltonian dynamics
+
+        IMPORTANT!!!
+        I am using the gradient of the density function and not
+        the gradient of the energy potential, which is usually used
+        in Hamiltonien methods (just a matter of sign)        
+        """
+
         self.stepsize = stepsize
         self.trajectory_length = trajectory_length
         self.log_prob = log_prob
         self.gradient_log_prob = gradient_log_prob
 
 
+    def build_chain(self, x_0, chain_length):
+        """To build a chain we need a starting point x_0 and tell the HMC
+        algorythm how long the chain should be.
+        """
 
-    def build_chain(self):
         n_accepted = 0
-        chain = [self.x_0]
+        chain = [x_0]
 
-        for i in range(self.chain_length):
+        for i in range(chain_length):
             state, accept = self.sample(chain[len(chain)-1])
             state = np.array(state)
-            chain = np.append(chain, [state], axis = 0)
+            chain = np.append(chain, [state], axis=0)
             n_accepted += accept
 
-        self.acceptance_rate= n_accepted/float(self.chain_length)
+        acceptance_rate = n_accepted/float(chain_length)
 
-        return chain
+        return chain, acceptance_rate
 
-    def get_acceptance_rate(self):
-        return self.acceptance_rate
 
     def log_p_acc(self, x_old, v_old, x_new, v_new):
-        def H(x,v): return -self.log_prob(x) + 0.5*np.sum(v**2)
-        return min(0,-(H(x_new, v_new) - H(x_old, v_old)))
+
+        def H(x,v):
+            return -self.log_prob(x) + 0.5*np.sum(v**2)
+
+        return min(0, -(H(x_new, v_new) - H(x_old, v_old)))
+
 
     def sample(self, x_old):
-        v_old = np.random.normal(size = len(x_old))
-        x_new, v_new = self.integrate(copy.copy(x_old), copy.copy(v_old))
 
-        accept = np.log(np.random.random()) < self.log_p_acc(x_old, v_old, x_new, v_new)
+        v_old = np.random.normal(size=len(x_old))
+        x_new, v_new = self.integrate(copy.copy(x_old), copy.copy(v_old))
+        accept = (np.log(np.random.random())
+                 < self.log_p_acc(x_old, v_old, x_new, v_new))
+
         if accept:
             return x_new, accept
         else:
@@ -50,35 +69,63 @@ class HMC:
 
     def integrate(self, x, v):
         ...
-class U7HMC (HMC):
-    def __init__(self, x_0, chain_length, log_prob, gradient_log_prob, hessian_log_prog, stepsize, trajectory_length):
-        super().__init__(x_0, chain_length, log_prob, gradient_log_prob, stepsize, trajectory_length)
+
+class U7HMC(HMC):
+
+    def __init__(self, log_prob, gradient_log_prob,
+                 hessian_log_prog, stepsize, trajectory_length):
+        """For our use, we want to compare two different integrator for
+        simulating the Hamiltonian dynamics. One is presented in this paper
+        https://arxiv.org/pdf/2007.05308.pdf and is just simply called u7.
+        As it is a fourth order intgrator it requires the hessian matrix of the
+        targeted log density as well.
+        """
+
+        super().__init__(log_prob, gradient_log_prob,
+        stepsize, trajectory_length)
+
         self.hessian_log_prog = hessian_log_prog
 
+
     def integrate(self, x, v):
-        v += 1/6 * self.stepsize * self.gradient_log_prob(x)
-        for i in range(self.trajectory_length -1):
-            x += 1/2 * v * self.stepsize
-            v += 2/3 * self.stepsize * (self.gradient_log_prob(x) + self.stepsize**2/24 * np.matmul(self.hessian_log_prog(x),self.gradient_log_prob(x)))
-            x = x + 1/2 * v * self.stepsize
-            v += 1/3 * self.stepsize * self.gradient_log_prob(x)
-        x += 1/2 * v * self.stepsize
-        v += 2/3 * self.stepsize * (self.gradient_log_prob(x) + self.stepsize**2/24 *np.matmul(self.hessian_log_prog(x),self.gradient_log_prob(x)))
-        x += 1/2 * v * self.stepsize
-        v += 1/6 * self.stepsize * self.gradient_log_prob(x)
+        """Note the different calculations to propose a new state of the
+        Hamiltonian dynamics. For further information see
+        https://arxiv.org/pdf/2007.05308.pdf.
+        """
+
+        v += 1./6 * self.stepsize * self.gradient_log_prob(x)
+
+        for i in range(self.trajectory_length-1):
+            x += 1./2 * v * self.stepsize
+            v += (2./3 * self.stepsize * (self.gradient_log_prob(x)
+                + self.stepsize**2/24
+                * np.matmul(self.hessian_log_prog(x),self.gradient_log_prob(x))))
+            x += 1./2 * v * self.stepsize
+            v += 1./3 * self.stepsize * self.gradient_log_prob(x)
+
+        x += 1./2 * v * self.stepsize
+        v += (2./3 * self.stepsize * (self.gradient_log_prob(x)
+            + self.stepsize**2./24
+            * np.matmul(self.hessian_log_prog(x),self.gradient_log_prob(x))))
+        x += 1./2 * v * self.stepsize
+        v += 1./6 * self.stepsize * self.gradient_log_prob(x)
+
         return x, v
 
-class LeapfrogHMC (HMC):
 
+class LeapfrogHMC(HMC):
 
     def integrate(self, x, v):
-        #I am using the gradient of the density function and not
-        #the gradient of the energy potential, which is used usually
-        #in Hamiltonien methodes (just a matter of sign)
-        v += 0.5 * self.stepsize * self.gradient_log_prob(x)
+        """The second integrator to which we compare the new u7 is the classical
+        Leapfrog."""
+
+        v += 1./2 * self.stepsize * self.gradient_log_prob(x)
+
         for i in range(self.trajectory_length-1):
-            x = x + self.stepsize*v
-            v = v + self.stepsize*self.gradient_log_prob(x)
+            x += self.stepsize * v
+            v += self.stepsize * self.gradient_log_prob(x)
+
         x += self.stepsize * v
-        v += 0.5 * self.stepsize * self.gradient_log_prob(x)
+        v += 1./2 * self.stepsize * self.gradient_log_prob(x)
+
         return x, v
