37e8ad3b8ab96e3683931f32b3116607ee57bbc9
ArneTillmann <arne.tillmann.vellmar@gmail.com>
2021-01-26 13:11:08 +0100

    quite nice result for two dim

--- comparison.py
+++ comparison.py
@@ -4,20 +4,25 @@ from hmc_sampler import U7HMC
 import gelman_rubin
 
 number_of_chains = 25
-chain_length = 500
-stepsize = 1.5
-trajectory_length = 10
+chain_length = 5000
+stepsize_u7 = 1.275
+stepsize_leapfrog = 0.715
+trajectory_length_u7 = 10
+trajectory_length_leapfrog = 10
 dimension = 2
 
 x_0 = np.zeros(dimension)   #may be chosen randomly for different chains
 mu = np.random.normal(size=dimension)
-A = np.random.rand(dimension, dimension)
-cov = np.dot(A, A.transpose())
-inv_cov = np.linalg.inv(cov)
-print(cov)
+# A = np.random.rand(dimension, dimension)
+# cov = np.dot(A, A.transpose())
+# inv_cov = np.linalg.inv(cov)
+# print(inv_cov)
+inv_cov = np.array([[ 2.70400247, -2.84656494],
+ [-2.84656494,  4.20325443]])
+print(inv_cov)
 def log_prob(x):
 
-    return -0.5 * np.sum((x-mu)@inv_cov@(x-mu))
+    return (-0.5 * (x-mu)@inv_cov@(x-mu))
 
 
 def gradient_log_prob(x):
@@ -32,7 +37,7 @@ def hessian_log_prog(x):
 
 """creating the chains for the different integrators respectively"""
 u7_HMCs = [U7HMC(log_prob, gradient_log_prob, hessian_log_prog,
-                 stepsize, trajectory_length) for _ in range(number_of_chains)]
+                 stepsize_u7, trajectory_length_u7) for _ in range(number_of_chains)]
 chainu7_HMCs = []
 acceptance_rate_list_u7 = []
 
@@ -44,8 +49,8 @@ for sampler in u7_HMCs:
 chainu7_HMCs = np.array(chainu7_HMCs)
 
 
-Leapfrog_HMCs = [LeapfrogHMC(log_prob, gradient_log_prob, stepsize,
-                             trajectory_length)
+Leapfrog_HMCs = [LeapfrogHMC(log_prob, gradient_log_prob, stepsize_leapfrog,
+                             trajectory_length_leapfrog)
                              for i in range(number_of_chains)]
 chainleapfrog_HMCs = []
 acceptance_rate_list_Leapfrog = []
