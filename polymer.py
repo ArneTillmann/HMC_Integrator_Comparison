8c6b8c25f5b4d06dc6db998b266bb01ad85f01db
ArneTillmann <arne.tillmann.vellmar@gmail.com>
2021-05-25 15:50:48 +0200

    blogpost update

--- polymer.py
+++ polymer.py
@@ -54,7 +54,57 @@ pair_combinations = np.array(list(combinations(range(number_of_monomere), 2)))
 inds_x = pair_combinations[:, 0]
 inds_y = pair_combinations[:, 1]
 
-x=np.abs(np.random.normal(0,6,(number_of_monomere,3))).ravel()
+x=np.abs(np.random.normal(0,5,(number_of_monomere,3))).ravel()
+# x = [[3.92776565e+00 6.32914833e+00 8.63926663e+00]
+#  [5.33246358e+00 5.42266052e-01 1.55669378e+00]
+#  [5.44660036e+00 1.09703276e+01 1.05734325e+00]
+#  [8.63752113e+00 9.60936126e-01 1.90726416e+00]
+#  [5.67858017e+00 3.85204630e+00 9.15805571e-02]
+#  [4.64714567e+00 4.35682451e-01 1.82413817e+00]
+#  [6.83294286e-01 4.32142721e+00 4.21013684e+00]
+#  [2.21348668e+00 1.40653129e+01 1.51088640e+00]
+#  [1.85387996e+00 2.88681163e+00 4.54883598e+00]
+#  [5.37156475e+00 1.19575502e+00 4.25272978e-01]
+#  [8.60731654e+00 8.00828616e-01 8.03548460e-01]
+#  [4.65184060e+00 5.88934330e+00 3.03460103e+00]
+#  [6.42895080e-01 6.49252673e+00 3.80216703e-01]
+#  [5.48043155e+00 4.89535516e+00 1.07421782e+00]
+#  [3.07661264e+00 8.03807008e+00 1.99541983e+00]
+#  [5.92486367e-01 1.53079377e+00 1.26607763e+00]
+#  [1.11341239e+00 7.36603801e-01 1.06952492e+00]
+#  [1.18866044e+01 7.87257456e-01 5.90299372e+00]
+#  [8.76532364e-01 1.40035243e+00 9.87259771e-01]
+#  [6.04135420e+00 3.39145124e+00 4.65621534e+00]
+#  [2.85741811e+00 3.29231799e+00 5.31105324e+00]
+#  [8.81458790e+00 7.43288641e+00 1.02910110e+00]
+#  [4.57376938e+00 1.29654011e+00 1.89026748e+00]
+#  [8.17672189e-01 1.99679876e+00 2.42272475e-01]
+#  [1.46560207e+00 8.77259394e+00 9.97868061e-01]
+#  [1.19392743e+00 3.30708078e+00 5.77382804e+00]
+#  [5.69758023e-01 2.45072772e+00 7.88091860e+00]
+#  [4.62748948e+00 2.29120419e+00 3.39046968e+00]
+#  [4.81775080e-01 1.05984238e+00 8.24891211e+00]
+#  [5.71660079e+00 1.09221609e+00 1.72650746e+01]
+#  [6.44625087e+00 5.39379576e+00 3.85436134e+00]
+#  [5.12586663e+00 6.93656203e+00 8.26549037e+00]
+#  [2.22317336e+00 1.42235941e+00 6.94234505e+00]
+#  [3.68946623e+00 3.77182456e+00 5.68195401e+00]
+#  [1.40703550e-01 2.75161557e+00 1.99356427e+00]
+#  [1.19841927e+00 3.07413076e+00 2.57494588e+00]
+#  [1.20884430e+01 4.19861829e+00 4.45632468e-01]
+#  [1.58224182e+00 8.53127564e+00 9.75336790e+00]
+#  [4.54942487e-01 2.97664447e+00 2.22136823e+00]
+#  [6.69620848e+00 6.40594252e+00 1.83654549e+00]
+#  [5.50747542e+00 4.22510544e+00 2.21709337e+00]
+#  [2.16275061e+00 7.52900493e+00 7.29132334e+00]
+#  [2.42701047e+00 1.09372696e+01 8.38293674e+00]
+#  [2.95808628e+00 1.20743271e+01 1.57284773e+00]
+#  [1.39118841e+01 3.51939701e+00 7.30769638e-01]
+#  [2.57571577e+00 6.27225514e+00 1.26420472e+00]
+#  [7.79529665e+00 4.07475887e+00 4.22849829e+00]
+#  [1.99550753e+00 2.74376430e+00 1.39083633e+00]
+#  [3.81853455e+00 6.51884933e+00 1.88719107e-03]
+#  [4.66021715e+00 7.89687751e+00 6.38243078e+00]]
 
 # b = int(np.floor(chain_length ** (1.0 / 3)))
 # a = int(np.floor(chain_length / b))
@@ -74,18 +124,20 @@ def plot_hist_data(chain, pair):
 
 
 if __name__ == "__main__":
-    chain_length = 5000
+    chain_length = 1000
     time = 10
     trajectory_length = 10
-    stepsize = 0.009
+    stepsize = 0.01
 
     leapfrog = LeapfrogHMC(log_posterior, gradient_log_posterior, stepsize, trajectory_length)
     chain_leapfrog , acceptancerate = leapfrog.build_chain(x, chain_length)
     print(acceptancerate)
 
+
     u7 = U7HMC(log_posterior, gradient_log_posterior, hessian_log_posterior, stepsize, trajectory_length)
     chain_u7, acceptancerate = u7.build_chain(x, chain_length)
     print(acceptancerate)
+
 
 
     chain_leapfrog.reshape(chain_length +1, number_of_monomere, dimension)
@@ -96,15 +148,12 @@ if __name__ == "__main__":
 
     # chain_u7.reshape(-1, number_of_monomere, dimension)
     # chain_u7 = chain_u7[:].reshape(chain_length+1, number_of_monomere, dimension)
-
-    write_ensemble(chain_leapfrog, "leapfrog_samples.pdb", center=False)
-    write_VMD_script("leapfrog_samples.pdb", mu_prior/2, chain_leapfrog.shape[1], "leapfrog_script.rc")
-
+    #
+    # write_ensemble(chain_leapfrog, "leapfrog_samples.pdb", center=False)
+    # write_VMD_script("leapfrog_samples.pdb", mu_prior/2, chain_leapfrog.shape[1], "leapfrog_script.rc")
+    #
 
     # write_ensemble(chain_u7, "u7_samples.pdb", center=False)
     # write_VMD_script("u7_samples.pdb", mu_prior/2, chain_u7.shape[1], "u7_script.rc")
-
-    plt.plot([log_posterior(x) for x in chain_leapfrog])
-    plt.show()
-
+    print(chain_leapfrog[0])
 ### vmd -e vmd_script.rc
